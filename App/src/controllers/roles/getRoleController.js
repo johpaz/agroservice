bfb063f682b15999768e551a20ea7317fbabf94e
johpaz <johpaz252@gmail.com>
2024-05-25 17:12:30 -0500

    feat Role Exclude Admin

--- App/src/controllers/roles/getRoleController.js
+++ App/src/controllers/roles/getRoleController.js
@@ -3,13 +3,16 @@ const Role = require('../../models/roleModel');
 // Controlador para obtener todos los roles
 const getAllRoles = async (req, res) => {
   try {
-    const roles = await Role.find();
+    let roles = await Role.find();
+    // Filtrar el rol 'Administrador' si está presente
+    roles = roles.filter(role => role.name !== 'Administrador');
     res.status(200).json(roles);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error en el servidor' });
   }
 };
+
 
 module.exports = {
   getAllRoles,
