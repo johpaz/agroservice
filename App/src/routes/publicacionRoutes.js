d9a66b73057c77de78b993482a7282fb8ce78bbb
johpaz <johpaz252@gmail.com>
2024-05-29 18:25:46 -0500

    feat publicaciones por productor

--- App/src/routes/publicacionRoutes.js
+++ App/src/routes/publicacionRoutes.js
@@ -3,16 +3,23 @@
 const {Router}= require('express');
 
 const {crearPublicacion,enviarOfertaAseguradora,enviarOfertaComprador,enviarOfertaTransportador} = require('../controllers/publicacion/publicacionController');
-const {getAllPublicacion,updatePublicacion,getPublicacionById} = require('../controllers/publicacion/getPublicacion')
+const {getAllPublicacion,updatePublicacion,getPublicacionById,getPublicacionByUser} = require('../controllers/publicacion/getPublicacion')
 
 const ofertasRoutes = Router();
+const ofertasConsultaRoutes = Router()
+
+
 // Rutas para ofertas
 ofertasRoutes.get('/', getAllPublicacion);
-ofertasRoutes.get('/', getPublicacionById);
+ofertasRoutes.get('/:id', getPublicacionById);
 ofertasRoutes.put('/:id', updatePublicacion);
 ofertasRoutes.post('/', crearPublicacion);
 ofertasRoutes.post('/:id/ofertas/compradores', enviarOfertaComprador);
 ofertasRoutes.post('/:id/oferta/transportador', enviarOfertaTransportador);
 ofertasRoutes.post('/:id/oferta/aseguradora', enviarOfertaAseguradora);
+ofertasConsultaRoutes.post('/', getPublicacionByUser);
 
-module.exports = ofertasRoutes;
+module.exports = {
+    ofertasRoutes,
+    ofertasConsultaRoutes
+};
