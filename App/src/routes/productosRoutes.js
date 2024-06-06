e69771fb1ac15549b4f52a5da40eb6c015e44e71
johpaz <johpaz252@gmail.com>
2024-06-06 11:13:05 -0500

    feat categorias y productos masivo

--- App/src/routes/productosRoutes.js
+++ App/src/routes/productosRoutes.js
@@ -4,10 +4,15 @@ const {handleCreateProductos} = require('../handler/productosHandler');
 const {getAllProductos} = require('../controllers/productos/getProductos')
 const {getProductosById} = require('../controllers/productos/getProductos')
 const {updateProductos} = require('../controllers/productos/getProductos')
+const {productosuploadHandler} = require ('../handler/productoMasivoHandler')
+const {categoriasMasivoHandler} = require ('../handler/categoriasMasivoHandler')
+const {handleUploadCategorias,handleUploadProductos} = require('../middleware/handleUpload')
 
 const productoRouter = Router();
 
 // Ruta para manejar la autenticación
+productoRouter.post('/categoriaMasivo', handleUploadCategorias,categoriasMasivoHandler);
+productoRouter.post('/productosMasivo', handleUploadProductos,productosuploadHandler);
 productoRouter.post('/', handleCreateProductos);
 productoRouter.get('/', getAllProductos);
 productoRouter.get('/:id', getProductosById);
